a1a5ea4b4de746ae2a051f4ba809d5aca99e63fd
Andrey Kuravlev <sector1200@gmail.com>
2020-04-06 11:37:11 +0500

    init commit

---
+++ App.tsx
@@ -0,0 +1,26 @@
+import * as React from "react";
+import { StyleSheet, View, Text } from "react-native";
+import { vw, vh, vmin, vmax } from "react-native-expo-viewport-units";
+
+import Controls from "./components/Controls";
+
+const App: () => React.ReactNode = () => (
+  <View style={styles.container}>
+    <View style={styles.lineStyle} />
+    <Controls />
+  </View>
+);
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "flex-end",
+  },
+
+  lineStyle: {
+    borderWidth: 1,
+    borderColor: "#7470ff",
+    marginHorizontal: vw(2),
+  },
+});
+
+export default App;
